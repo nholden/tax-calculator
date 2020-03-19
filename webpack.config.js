b3b365e195deb929d1eec965fd43a876152de0d2
Nick Holden <nick.r.holden@gmail.com>
2020-03-19 12:23:42 -0700

    Install Tailwind CSS
    https://paramagicdev.github.io/my-blog/javascript/tailwindWebpackPostCSS/

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path")
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  mode: "production",
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          { loader: "css-loader", options: { importLoaders: 1 } },
+          "postcss-loader",
+        ],
+      },
+    ],
+  },
+}
