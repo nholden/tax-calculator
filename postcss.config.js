bf353d4fb05e3df2a4c4f632aae3c5e1fe374aee
Nick Holden <nick.r.holden@gmail.com>
2020-03-19 12:39:47 -0700

    Install PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,18 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  // Specify the paths to all of the template files in your project 
+  content: [
+    './dist/**/*.html',
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
 module.exports = {
   plugins: [
     require("tailwindcss")("./tailwind.config.js"),
     require("autoprefixer"),
+    purgecss,
   ],
 }
